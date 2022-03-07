8a32d95c977f76cf9a9783182e28960da589c21d
Supercam19 <79729769+supercam19@users.noreply.github.com>
2022-03-06 20:44:42 -0500

    github stop being silly >:(

--- scripts/functions.py
+++ scripts/functions.py
@@ -1,27 +1,10 @@
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-enchantments = ("sharpness", "smite", "bane_of_arthropods", "knockback", "fire_aspect",
-                "sweeping_edge", "protection", "fire_protection", "feather_falling", "blast_protection",
-                "projectile_protection", "respiration", "aqua_affinity", "depth_strider",
-                "frost_walker", "soul_speed", "efficiency", "silk_touch", "unbreaking",
-                "looting", "fortune", "luck_of_the_sea", "lure", "power", "flame", "punch",
-                "infinity", "thorns", "mending", "binding_curse", "vanishing_curse", "loyalty",
-                "impaling", "riptide", "channeling", "multishot", "quick_charge", "piercing")
-=======
 import pandas as pd
-=======
-import pandas as pd
-
-df = pd.read_excel('Enchantment Details.xlsx', sheet_name='Sheet1')
-enchantments = df['Enchantment'].tolist()
->>>>>>> 0fe34e1b33316b2e9d345f43865cf02c12ea2fff
 
 df = pd.read_excel('Enchantment Details.xlsx', sheet_name='Sheet1')  # Creates a DataFrame from the spreadsheet
 enchantments = df['Enchantment'].tolist()  # Converts the spreadsheet column "Enchantment" to a list so it can be indexed later
->>>>>>> Stashed changes
 
 for enchant in enchantments:  # Loop through each enchantment file
-    new_file = open(f'output/{enchant}.mcfunction', 'w')  # Opens the file or creates one if it doesn't exist
+    new_file = open(f'output/{enchant}.mcfunction', 'w')  # Opens the file or creates one if it doesn't
     new_file.truncate()  # Removes contents of the file
     for slot in range(36):  # Writes the following string 36 times (for each inventory slot)
         new_file.write(f'execute if entity @s[nbt={{Inventory:[{{Slot:{slot}b, id:"minecraft:enchanted_book", tag:{{StoredEnchantments: [{{id: "minecraft:{enchant}"}}]}}}}]}}] run item modify entity @s container.{slot} enchdetails:{enchant}\n')
--- scripts/item_modifiers.py
+++ scripts/item_modifiers.py
@@ -1,25 +1,14 @@
 import pandas as pd
 
-<<<<<<< HEAD
 df = pd.read_excel('Enchantment Details.xlsx', sheet_name='Sheet1')  # Creates a DataFrame from the spreadsheet
-
 # Creates a list out of the three columns so they can be indexed later
-=======
-df = pd.read_excel('Enchantment Details.xlsx', sheet_name='Sheet1')
->>>>>>> 0fe34e1b33316b2e9d345f43865cf02c12ea2fff
 enchantments = df['Enchantment'].tolist()
 descriptions = df['Description'].tolist()
 applications = df['Applications'].tolist()
 
 
-<<<<<<< HEAD
 for enchant in range(len(enchantments)):  # Loop through each enchantment file
     new_file = open(f'output/{enchantments[enchant]}.json', 'w')  # Opens the file or creates one if it doesn't exist
     new_file.truncate()  # Removes the contents of the file
-=======
-for enchant in range(len(enchantments)):
-    new_file = open(f'output/{enchantments[enchant]}.json', 'w')
-    new_file.truncate()
->>>>>>> 0fe34e1b33316b2e9d345f43865cf02c12ea2fff
     new_file.write(f'{{\n\t\"function\": \"set_lore\",\n\t\"lore\": [{{\"text\":\"{descriptions[enchant]}\",\"italic\":false,\"color\":\"#e699e6\"}},{{\"text\":\"For: {applications[enchant]}\",\"italic\":true,\"color\":\"#bfbfbf\"}}],\n\t\"replace\": \"true\"\n}}')
     new_file.close()
